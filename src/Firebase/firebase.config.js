83fae057e63a8b9ba1a4397e63049b251cc1902f
JaznanOfficial <jaznanofficial@gmail.com>
2021-10-27 16:29:46 +0600

    first commit

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -1,11 +1,11 @@
-console.log(process.env);
+
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_API_KEY,
-    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_APP_ID,
-};
+    apiKey: "AIzaSyBW8_oid8rp2Oo6XKk-uE1cRsQ0dRl5RJk",
+    authDomain: "ema-jhon-node-mongo.firebaseapp.com",
+    projectId: "ema-jhon-node-mongo",
+    storageBucket: "ema-jhon-node-mongo.appspot.com",
+    messagingSenderId: "501241265426",
+    appId: "1:501241265426:web:cc518db3ce28a664c007cb"
+  };
 
 export default firebaseConfig;
